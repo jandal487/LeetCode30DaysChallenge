28fdffa8067c4e3d1bd3f5c5a73c27f5b776bc31
jandal487 <jandal487@gmail.com>
2020-04-08 03:37:13 +0200

    pushing week1 solutions

--- week1.py
+++ week1.py
@@ -80,10 +80,40 @@ class Solution:
             return 0
         return 0
 
+    # 6. Group Anagrams
+    def groupAnagrams(self, strs):
+        temp_dict = {}
+        
+        for item in strs:
+            sortedItem = sorted(item)
+            hashKey = ''.join(sortedItem)
+            if hashKey in temp_dict:
+                temp_dict[hashKey].append(item)
+            else:
+                temp_dict[hashKey] = [item]
+
+        return [value for value in temp_dict.values()]
+
+    # 7. Counting Elements
+    def countElements(self, arr):
+        temp_dict = {}
+        for x in arr:
+            temp_dict[x] = 1
+        
+        count = 0
+        for x in arr:
+            if (x+1 in temp_dict):
+                count = count + 1
+
+        return count
+
+
 solObj = Solution()
 #solObj.singleNumber(221)
 #solObj.isHappy(19)
 #solObj.maxSubArray([8,-19,5,-4,20])
 #solObj.moveZeroes([0,1,0,3,12])
-solObj.maxProfit([7,6,4,3,1])
+#solObj.maxProfit([7,6,4,3,1])
+#solObj.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
+solObj.countElements(  [1,3,2,3,5,0])
 
